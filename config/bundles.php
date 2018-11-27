38faed4b6161a31e7ca1949de5a5ae99fab0e8e1
Jordan <thejordan01@gmail.com>
2018-11-27 12:34:04 +0100

    Try to fix composer conflict for travis

--- config/bundles.php
+++ config/bundles.php
@@ -9,5 +9,4 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
